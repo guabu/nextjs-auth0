89bd481a069b7f5b777da8e08f3cc108f94e64e0
guabu <135956181+guabu@users.noreply.github.com>
2024-09-02 22:25:10 +0200

    chore: define user type returned from useUser hook

--- src/client/hooks/use-user.ts
+++ src/client/hooks/use-user.ts
@@ -2,9 +2,26 @@
 
 import useSWR from "swr"
 
+// TODO: allow a developer to define their own user interface
+// since it can be overridden. Same for the session data interface.
+interface User {
+  sub: string
+  name?: string
+  nickname?: string
+  given_name?: string
+  family_name?: string
+  picture?: string
+  email?: string
+  email_verified?: boolean
+  org_id?: string
+
+  [key: string]: any
+}
+
 export function useUser() {
-  const { data, error, isLoading } = useSWR("/auth/profile", (...args) =>
-    fetch(...args).then((res) => res.json())
+  const { data, error, isLoading } = useSWR<User, {}, string>(
+    "/auth/profile",
+    (...args) => fetch(...args).then((res) => res.json())
   )
 
   return {
